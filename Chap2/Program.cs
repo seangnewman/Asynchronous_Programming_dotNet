dd37e5fabe6805d0eb21440d20696f79e2d6376c
Sean Newman <seangnewman@msn.com>
2018-12-03 14:28:36 -0700

    Initial Commit

---
+++ Chap2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartMethod();
+        }
+
+        private static void StartMethod()
+        {
+            Thread monitorThread = new Thread(new ThreadStart(MonitorNetwork));
+            monitorThread.Start();
+        }
+
+        private static void MonitorNetwork()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
